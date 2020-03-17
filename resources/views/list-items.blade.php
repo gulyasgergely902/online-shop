252a54c67a629437ecdc98781e166bb5d3eb0565
Gulyas Gergely <gulyas.gergely@hotmail.com>
2020-03-17 20:13:07 +0100

    OS-002 added db connection, created items listing page

--- resources/views/list-items.blade.php
+++ resources/views/list-items.blade.php
@@ -65,9 +65,24 @@
 						<h1>Items</h1>
 						<p>In: Home > {{ $category_data->name }}</p>
 					</div>
-					<div>
+					<div class="item-card-wrapper">
 						@foreach ($items as $item)
-							{{ $item->name }}
+							<div class="item-card">
+								<div class="item-image" style="background: url(../{{ $item->image_link }}) no-repeat center center !important;">
+								</div>
+								<div class="item-details">
+									<h2>{{ $item->name }}</h2>
+									<p>{{ $item->description }}</p>
+								</div>
+								<div class="item-price">
+									@if($item->sale == 1)
+									<h5 class="item-old-price">{{ $item->price }}&nbspFt</h5>
+									<h2 class="item-sale-price">{{ $item->sale_price }}&nbspFt</h2>
+									@else
+									<h2 class="item-normal-price">{{ $item->price }}&nbspFt</h2>
+									@endif
+								</div>
+							</div>
                         @endforeach
 					</div>
 				</div>
