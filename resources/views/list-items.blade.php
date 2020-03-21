0a103681440e96a5e973fcc0b71c75e801e28572
Gulyas Gergely <gulyas.gergely@hotmail.com>
2020-03-21 15:30:49 +0100

    OS-004 implemented user register, login and profile

--- resources/views/list-items.blade.php
+++ resources/views/list-items.blade.php
@@ -1,100 +1,31 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layout')
 
-        <title>Online Shop | Items</title>
-
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-        <!-- styles -->
-        <link rel="stylesheet" type="text/css" href="../style.css">
-        
-        <!-- jQuery -->
-        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-
-        <!-- custom script -->
-    	<script src="../script.js"></script>
-    </head>
-    <body>
-		<div class="spinner" id="spinner"></div>
-		<!-- Bootstrap Grid -->
-		<div class="container-fluid" id="container-fluid">
-			<div class="row">
-				<div class="col-md-12">
-					<nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-navbar-collapse-1">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-						<a class="navbar-brand" href="/">Online Shop</a>
-						<div class="collapse navbar-collapse" id="bs-navbar-collapse-1">
-							<ul class="navbar-nav">
-								<li class="nav-item">
-									 <a class="nav-link" href="/">Home</a>
-								</li>
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" data-toggle="dropdown">Categories</a>
-									<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-										<a class="dropdown-item" href="/list-items/discounted">Discounted</a>
-										<a class="dropdown-item" href="/list-items/lowest">All time lowest</a>
-										<div class="dropdown-divider"></div>
-										<a class="dropdown-item" href="/list-items/clothing">Clothing</a>
-										<a class="dropdown-item" href="/list-items/electronics">Electronics</a>
-										<a class="dropdown-item" href="/list-items/home-garden">Home & Garden</a>
-										<a class="dropdown-item" href="/list-items/pets">Pets</a>
-										<a class="dropdown-item" href="/list-items/sports">Sports</a>
-										<a class="dropdown-item" href="/list-items/health">Health</a>
-										<a class="dropdown-item" href="/list-items/car-accessories">Car accessories</a>
-										<a class="dropdown-item" href="/list-items/tools">Tools</a>
-									</div>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link disabled" href="/search">Search</a>
-								</li>
-							</ul>
-							<ul class="navbar-nav ml-md-auto">
-								<li class="nav-item">
-									 <a class="nav-link" href="/signup">Login</a>
-								</li>
-							</ul>
-						</div>
-					</nav>
-					<div class="jumbotron">
-						<h1>Listing items</h1>
-						<p>In: Home > {{ $category_data->name }}</p>
-					</div>
-					<div class="item-card-wrapper">
-						@foreach ($items as $item)
-							<div class="item-card">
-								<div class="item-image" style="background: url(../{{ $item->image_link }}) no-repeat center center !important;">
-								</div>
-								<div class="item-details">
-									<h2>{{ $item->name }}</h2>
-									<p>{{ $item->description }}</p>
-									<!-- @if($category_data->id == "discounted")
-										<p>{{ $item->category_id }}</p>
-									@endif -->
-								</div>
-								<div class="item-price">
-									@if($item->sale == 1)
-									<h5 class="item-old-price">{{ $item->price }}&nbspFt</h5>
-									<h2 class="item-sale-price">{{ $item->sale_price }}&nbspFt</h2>
-									@else
-									<h2 class="item-normal-price">{{ $item->price }}&nbspFt</h2>
-									@endif
-								</div>
-							</div>
-                        @endforeach
-					</div>
-				</div>
+@section ('content')
+<div class="jumbotron">
+	<h1>Listing items</h1>
+	<p>In: Home > {{ $category_data->name }}</p>
+</div>
+<div class="item-card-wrapper">
+	@foreach ($items as $item)
+		<div class="item-card">
+			<div class="item-image" style="background: url(../{{ $item->image_link }}) no-repeat center center !important;">
+			</div>
+			<div class="item-details">
+				<h2>{{ $item->name }}</h2>
+				<p>{{ $item->description }}</p>
+				<!-- @if($category_data->id == "discounted")
+					<p>{{ $item->category_id }}</p>
+				@endif -->
+			</div>
+			<div class="item-price">
+				@if($item->sale == 1)
+				<h5 class="item-old-price">{{ $item->price }}&nbspFt</h5>
+				<h2 class="item-sale-price">{{ $item->sale_price }}&nbspFt</h2>
+				@else
+				<h2 class="item-normal-price">{{ $item->price }}&nbspFt</h2>
+				@endif
 			</div>
 		</div>
-
-		<!-- End of Bootstrap Grid -->
-
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    </body>
-</html>
+	@endforeach
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/list-items/{category}', 'ItemsController@show');
+Route::get('/list-items/{category}', 'ShopController@showItemsList');
+Auth::routes();
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Route::get('/profile', 'UserController@showProfile');
