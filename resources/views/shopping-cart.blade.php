1ff2c4a01766aca30a7ed01f18af1a0fce87c461
Gulyas Gergely <gulyas.gergely@hotmail.com>
2020-04-10 11:52:25 +0200

    OS-007 fixed search page and shopping cart page where item list cards were broken

--- resources/views/shopping-cart.blade.php
+++ resources/views/shopping-cart.blade.php
@@ -6,23 +6,25 @@
 </div>
 <div class="item-card-wrapper">
 	@foreach ($items as $item)
-		<div class="item-card">
-			<div class="item-image" style="background: url(../{{ $item->image_link }}) no-repeat center center !important;">
-			</div>
-			<div class="item-details">
-				<h2>{{ $item->name }}</h2>
-				<p>{{ $item->description }}</p>
-			</div>
-			<div class="item-price">
-				@if($item->sale == 1)
-				<h5 class="item-old-price">{{ $item->price }}&nbspFt</h5>
-				<h2 class="item-sale-price">{{ $item->sale_price }}&nbspFt</h2>
-				@else
-				<h2 class="item-normal-price">{{ $item->price }}&nbspFt</h2>
-				@endif
-			</div>
-			<div class="item-buttons">
-				<a href="/remove-from-cart/{{ $item->id }}" class="btn btn-danger" role="button"><i class="fas fa-trash-alt"></i>&nbspRemove</a>
+		<div class="card mb-3">
+			<a class="text-decoration-none text-body" href="/item-details/{{ $item->id }}"><div class="card-header">{{ $item->name }}</div></a>
+			<div class="card-body list-items-card">
+				<div class="item-image" style="background: url(../{{ $item->image_link }}) no-repeat center center !important;">
+				</div>
+				<div class="item-details">
+					<p>{{ $item->description }}</p>
+				</div>
+				<div class="item-price">
+					@if($item->sale == 1)
+					<h5 class="item-old-price">{{ $item->price }}&nbspFt</h5>
+					<h2 class="item-sale-price">{{ $item->sale_price }}&nbspFt</h2>
+					@else
+					<h2 class="item-normal-price">{{ $item->price }}&nbspFt</h2>
+					@endif
+				</div>
+				<div class="item-buttons">
+					<a href="/remove-from-cart/{{ $item->id }}" class="btn btn-danger" role="button"><i class="fas fa-trash-alt"></i>&nbspRemove</a>
+				</div>
 			</div>
 		</div>
 	@endforeach
